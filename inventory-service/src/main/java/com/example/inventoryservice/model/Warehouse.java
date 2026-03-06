3acd06726de06e1cefeb450385d1fd011b390ab7
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2026-03-07 00:01:42 +0530

    feat: add Warehouse entity with attributes for code, name, location, and capacity

--- inventory-service/src/main/java/com/example/inventoryservice/model/Warehouse.java
+++ inventory-service/src/main/java/com/example/inventoryservice/model/Warehouse.java
@@ -1,4 +1,34 @@
 package com.example.inventoryservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "warehouses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Warehouse {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String warehouseCode;
+
+    @Column(nullable = false)
+    private String warehouseName;
+
+    @Column(nullable = false)
+    private String location;
+
+    // tracking if a warehouse is full
+    private Integer maxCapacity;
+
 }
